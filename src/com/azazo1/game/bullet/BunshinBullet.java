0d0de9b9cf4d4c7f831e3018381ee5aaed228465
azazo1 <azazo1@qq.com>
2023-01-22 00:40:44 +0800

    1. 削弱了分身子弹 (限制最大分身次数) 2. 分身子弹现在有旋转效果

--- src/com/azazo1/game/bullet/BunshinBullet.java
+++ src/com/azazo1/game/bullet/BunshinBullet.java
@@ -1,5 +1,6 @@
 package com.azazo1.game.bullet;
 
+import com.azazo1.util.AtomicDouble;
 import com.azazo1.util.Tools;
 import org.jetbrains.annotations.NotNull;
 
@@ -7,10 +8,13 @@ import java.awt.*;
 import java.util.Random;
 
 /**
- * 分身子弹, 碰到墙有一定概率分身, 但所有分身都会在同一时间消失(除非有的分身撞墙太多了)
+ * 分身子弹, 碰到墙有一定概率分身, 但所有分身都会在同一 较短 时间消失(除非有的分身撞墙太多了/击中坦克)
  */
 public class BunshinBullet extends BulletBase {
     public static final String imgFile = "img/BunshinBullet.png";
+    protected final AtomicDouble theta = new AtomicDouble(0); // 不断递增用于形成旋转效果
+    public final int maxBunshinDepth = 3; // 最大分身深度
+    protected final int depth; // 不断递增用于形成旋转效果
 
     {
         rawImg = Tools.loadImg(imgFile);
@@ -29,9 +33,12 @@ public class BunshinBullet extends BulletBase {
                             if (new Random().nextDouble() > 0.65) { // 复制有一定概率, 不然太多了
                                 return;
                             }
+                            if (depth >= maxBunshinDepth) {
+                                return;
+                            }
                             bulletGroup.addBullet(
                                     new BunshinBullet((int) rect.getCenterX(), (int) rect.getCenterY(),
-                                            rawOrientation + Math.PI / 3, lifeModule.createdTime.get())
+                                            rawOrientation + Math.PI / 3, lifeModule.createdTime.get(), depth+1)
                             );
                         }
                     });
@@ -42,13 +49,26 @@ public class BunshinBullet extends BulletBase {
         };
     }
 
-    public BunshinBullet(int centerX, int centerY, double orientation) {
-        super(centerX, centerY, orientation);
+    @Override
+    protected void paint(Graphics graphics) {
+        double rawOrientation = orientation.get();
+        orientation.set(rawOrientation + theta.getAndSet(theta.get() + 15 * Math.PI / 360)); // 旋转效果
+        super.paint(graphics);
+        orientation.set(rawOrientation);
     }
 
-    public BunshinBullet(int centerX, int centerY, double orientation, long createdTime) {
+    public BunshinBullet(int centerX, int centerY, double orientation) {
+        super(centerX, centerY, orientation);
+        depth = 0;
+    }
+
+    /**
+     * @param depth 要创建的子弹所处 分身深度
+     */
+    public BunshinBullet(int centerX, int centerY, double orientation, long createdTime, int depth) {
         super(centerX, centerY, orientation);
         lifeModule.createdTime.set(createdTime);
+        this.depth = depth;
     }
 
 }
